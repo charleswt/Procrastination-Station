a922823948798c4ba6a4fa20447df022e6898933
Manya Naranzogt <manya.naranzogt@gmail.com>
2024-04-11 11:52:09 -0700

    added page contents with component links

---
+++ client/src/pages/games.jsx
@@ -0,0 +1,11 @@
+import { Game } from '../components/gameDisplayCards'
+
+const Games = () => {
+    return (
+        <div> Home page 
+            <Game />
+        </div>
+    )
+}
+
+export default Games;
