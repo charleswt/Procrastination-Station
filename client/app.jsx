a59139c558718d0afdaff1de63a0462d5b09811b
Manya Naranzogt <manya.naranzogt@gmail.com>
2024-04-10 20:48:43 -0700

    populated app.jsx with apollo wrapper and included authorization code snippet

---
+++ client/app.jsx
@@ -0,0 +1,46 @@
+import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Nav from './src/components/nav';
+import Footer from './src/components/footer'
+
+// setting up httpLink
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// adds auth token from local storage to bearer header specified
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// ApolloClient is instantiated with authLink added to httplink and new memory cache created
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// react components wrapped in Apollo server
+function App() {
+  return (
+    <ApolloProvider client={client}>
+        <Nav />
+        <Outlet />
+        <Footer />
+    </ApolloProvider>
+  );
+}
+
+export default App;
