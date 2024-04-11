33b38cc41db7708c8c816b443ac50144e06ce010
ct <charleswtiffany@gmail.com>
2024-04-10 23:11:42 -0700

    changed typedef code added some code to auth and delted routes controllers and disconnected routes in server.js

--- server/routes/api/saleRoutes.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-const { getPosts } = require('../../controllers/saleController');
-
-
-module.exports = router;
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const api = require('./api');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/api', api);
-router.use('/', homeRoutes)
-
-module.exports = router;
