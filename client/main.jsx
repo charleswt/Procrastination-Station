dc1d9b122e08099864433f38320d305777c28a39
Manya Naranzogt <manya.naranzogt@gmail.com>
2024-04-10 20:35:36 -0700

    added react router paths and profile.jsx page :

---
+++ client/main.jsx
@@ -0,0 +1,50 @@
+import ReactDOM from 'react-dom/client'
+import './style.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// pages imports
+import Dino from './src/pages/dino.jsx'
+import Games  from './src/pages/games.jsx'
+import Pong from './src/pages/pong.jsx'
+import Snake from './src/pages/snake.jsx'
+import TTT from './src/pages/ttt.jsx'
+import Error from './src/pages/error.jsx'
+import Profile from './src/pages/profile.jsx'
+
+// router set up
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      error: <Error />,
+      children: [
+        {
+          index: true, 
+          element: <Games />
+        },
+        {
+            path: '/profile/:id',
+            element: <Profile />
+          },
+        {
+          path: '/snake',
+          element: <Snake />
+        }, {
+          path: '/pong',
+          element: <Pong />
+        }, {
+          path: '/dino',
+          element: <Dino />
+        },
+        {
+            path: '/ttt',
+            element: <TTT />
+          }
+      ]
+    }
+  ]);
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+  )
+  
