05672bef7447b15237fd5e8bb94aea9ba01777c5
Manya Naranzogt <manya.naranzogt@gmail.com>
2024-04-14 19:08:57 -0700

    added dino game and score view

--- client/src/components/dino/dino.jsx
+++ client/src/components/dino/dino.jsx
@@ -1,8 +1,18 @@
 import './style.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function DinoGame(){
-    const [jump, setJump] = useState("")
+
+    let [jump, setJump] = useState("")
+    let [score, setScore] = useState(0)
+    let dinoScore = ()=> {localStorage.setItem('dinoScore', score);} 
+    let savedScore = localStorage.getItem('dinoScore')
+   
+    useEffect(()=>{
+        if (score > savedScore){
+            dinoScore(score)
+        }
+    }, [score])
 
     function keyDown(){
         if (jump === ""){
@@ -13,23 +23,38 @@ export default function DinoGame(){
             setJump("")
         }, 300)
     }
-
+// checking positions of dino and cactus if they collide, send an alert 
     let isAlive = setInterval(function(){
         // getting dino Y position and cactus x position
         let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
         let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
 
         //detect collision
-        if(cactusLeft < 50 && cactusLeft >0 && dinoTop >= 140){
+        if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
             // collision
-            alert('You died!')
+            alert('you died')
+            setScore(0)
+            return false
+
+        } else {
+            setScore((score+1));
+            return true;
         }
-    },500)
+    },700)
+// adding to score if alive checking each second
+    setInterval(function(){
+        if (isAlive){
+            setScore((score+1));
+        } 
+    }, 700)
+
 
     return (
         <div className="game">
             <div className= {jump} id="dino" onClick={keyDown}></div>
             <div id="cactus"></div>
+            <br></br>
+            <div id="score"> High Score: {savedScore}</div>
         </div>
     )
 }
