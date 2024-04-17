ea242d3e7bb2cb94fef5df5a213ad2949b9d8e51
Robert Solorzano <robertsolorzano510@gmail.com>
2024-04-16 18:36:36 -0700

    Updated path in main for snake, updated footer position

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,8 +1,17 @@
-
 export default function Footer(){
+    const footerStyle = {
+        position: 'fixed', // or 'absolute' if preferred
+        bottom: 0,
+        width: '100%',
+        backgroundColor: '#f0f0f0',
+        padding: '10px',
+        textAlign: 'center',
+        zIndex: 1, // Ensure the footer is behind the game board
+    };
+
     return (
-        <footer>
+        <footer style={footerStyle}>
             Footer here
-            </footer>
-    )
-}; 
+        </footer>
+    );
+};
