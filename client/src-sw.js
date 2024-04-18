5e7c3b8d3066591044f1fb2f2ac0f326386a3721
ct <charleswtiffany@gmail.com>
2024-04-18 14:44:04 -0700

    added webpack and service worker. injectmanifest error

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,18 +1,18 @@
-// const { StaleWhileRevalidate } = require('workbox-strategies');
-// const { registerRoute } = require('workbox-routing');
-// const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-// const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-// precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(self.__WB_MANIFEST);
 
-// registerRoute(
-//   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-//   new StaleWhileRevalidate({
-//     cacheName: 'asset-cache',
-//     plugins: [
-//       new CacheableResponsePlugin({
-//         statuses: [0, 200],
-//       }),
-//     ],
-//   })
-// );
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
